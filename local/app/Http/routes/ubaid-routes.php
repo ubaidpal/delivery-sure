4f90922b21390d68204e23347a220909f246fa73
Ubaid Pal <81699804+ubaidpal@users.noreply.github.com>
2021-04-23 15:44:55 -0400

    Update ubaid-routes.php

--- local/app/Http/routes/ubaid-routes.php
+++ local/app/Http/routes/ubaid-routes.php
@@ -1,16 +1,7 @@
 <?php
 // Withdraws Routes
 
-Route::group(['middleware' => ['data','auth']], function () {
-    Route::get('/withdrawls', 'ManagementController@requestWithdrawal');
-    Route::get('/getBankAccount', 'ManagementController@getBankAccount');
-    Route::post('/addBankAccount', 'ManagementController@addBankAccount');
 
-    Route::post('/sendWithdrawalRequest', 'ManagementController@sendWithdrawalRequest');
-    Route::get('cancelWithdrawalRequest/{id}', 'ManagementController@cancelWithdrawalRequest');
-
-    //Route::get('dashboard/{page?}', 'UserController@dashboard');
-});
 
 Route::group(['middleware' => ['auth', 'data']], function () {
 
